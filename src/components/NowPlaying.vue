ba1a8a4a58e2d5fd6efeed4fbeef4e3c560c3d59
matssee <mats@leval.se>
2022-11-05 15:12:07 +0100

    Popularity text.

--- src/components/NowPlaying.vue
+++ src/components/NowPlaying.vue
@@ -16,7 +16,7 @@
         <h1 class="now-playing__track" v-text="player.trackTitle"></h1>
         <h2 class="now-playing__artists" v-text="getTrackArtists"></h2>
         <h2 class="now-playing__artists" v-text="player.trackAlbum.title"></h2>
-        <h3 class="now-playing__artists" v-text="Popularity: player.popularity"></h2>
+        <h3 class="now-playing__artists" prefix="Popularity: " v-text="player.popularity"></h2>
         <h3 class="now-playing__artists" v-text="player.trackAlbum.releaseDate"></h2>
       </div>
     </div>
